dd016c3a05fa214d01b315e08c3df429a4f6b2d0
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-01-27 11:57:05 +0100

    fix: add generic types to event abstract for engine

--- src/engine/event/EventAbstract.ts
+++ src/engine/event/EventAbstract.ts
@@ -1,12 +1,12 @@
 import { Effect, Event, EventOptions, Validator } from './types';
 
-abstract class EventAbstract implements Event {
+abstract class EventAbstract<O extends EventOptions = EventOptions> implements Event<O> {
   public static event: string;
 
-  abstract effects: Effect<EventOptions>[];
-  abstract validators: Validator<EventOptions>[];
+  abstract effects: Effect<O>[];
+  abstract validators: Validator<O>[];
 
-  abstract handler(options?: EventOptions): void;
+  abstract handler(options: O): void;
 }
 
 export default EventAbstract;
