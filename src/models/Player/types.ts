eecd4b36af1863791ae6a80e743d68327deabefe
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-01-31 10:41:53 +0100

    feat: add and render player vitals

--- src/models/Player/types.ts
+++ src/models/Player/types.ts
@@ -1,5 +1,13 @@
 import { Position } from '../Position';
 
+export interface Vitals {
+  fullness: number;
+  health: number;
+  hydration: number;
+  warmth: number;
+}
+
 export interface Player {
   position: Position;
+  vitals: Vitals;
 }
