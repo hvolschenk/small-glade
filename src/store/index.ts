2d834819c6ef37644123832cdbdbfc54950af13b
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-02-05 22:46:44 +0100

    chore: define starting inventory

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import startingInventory from '~/src/models/Inventory/StartingInventory';
 import smallGlade from '~/src/models/Map/SmallGlade';
 import baseOutfit from '~/src/models/Outfit/BaseOutfit';
 
@@ -10,6 +11,10 @@ export type Dispatch = typeof store.dispatch;
 export type GetState = typeof store.getState;
 
 export const store = configureStore<RootState>({
-  preloadedState: { map: smallGlade, outfit: baseOutfit },
+  preloadedState: {
+    inventory: startingInventory,
+    map: smallGlade,
+    outfit: baseOutfit,
+  },
   reducer: reducers,
 });
