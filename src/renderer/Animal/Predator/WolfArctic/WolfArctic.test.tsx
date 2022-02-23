3583962c93f6242ffd39a8709c70e99ff3af073e
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-02-23 22:09:07 +0100

    chore: move predator aggro indicator to parent renderer

--- src/renderer/Animal/Predator/WolfArctic/WolfArctic.test.tsx
+++ src/renderer/Animal/Predator/WolfArctic/WolfArctic.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Predator } from '~/src/models/Animal/types';
 import wolfArctic from '~/src/models/Animal/WolfArctic';
 import { Position } from '~/src/models/Position';
 import { render, RenderResult } from '~/src/testing';
@@ -9,28 +8,12 @@ import WolfArctic from './index';
 
 const position: Position = { left: 3, top: 11 };
 
-describe('When the predator is not aggroed', () => {
-  let wrapper: RenderResult;
+let wrapper: RenderResult;
 
-  beforeEach(() => {
-    const predator: Predator = { ...wolfArctic, isAggroed: false };
-    wrapper = render(<WolfArctic animal={predator} position={position} />);
-  });
-
-  test('Does not show the aggro indicator', () => {
-    expect(wrapper.queryByTestId('animal__wolf-arctic__aggro-indicator')).not.toBeInTheDocument();
-  });
+beforeEach(() => {
+  wrapper = render(<WolfArctic animal={wolfArctic} position={position} />);
 });
 
-describe('When the predator is aggroed', () => {
-  let wrapper: RenderResult;
-
-  beforeEach(() => {
-    const predator: Predator = { ...wolfArctic, isAggroed: true };
-    wrapper = render(<WolfArctic animal={predator} position={position} />);
-  });
-
-  test('Shows the aggro indicator', () => {
-    expect(wrapper.queryByTestId('animal__wolf-arctic__aggro-indicator')).toBeInTheDocument();
-  });
+test('Vanity test', () => {
+  expect(wrapper.queryByTestId('animal__wolf-arctic')).toBeInTheDocument();
 });
--- src/renderer/Animal/Predator/WolfArctic/index.tsx
+++ src/renderer/Animal/Predator/WolfArctic/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { PredatorRendererProps } from '../types';
 
-const WolfArctic: React.FC<PredatorRendererProps> = ({ animal }) => (
+const WolfArctic: React.FC<PredatorRendererProps> = ({ children }) => (
   <div className="wolf-arctic" data-testid="animal__wolf-arctic">
-    {animal.isAggroed && <span data-testid="animal__wolf-arctic__aggro-indicator">!</span>}
+    {children}
   </div>
 );
 
