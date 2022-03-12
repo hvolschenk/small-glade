40c1405111eede40c36f9f930bc2ac8a8463c91a
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-03-12 22:17:34 +0100

    feat: add wool socks as an item

---
+++ src/models/Item/Clothing/SocksWool.ts
@@ -0,0 +1,15 @@
+import { ClothingSocks } from './types';
+
+const socksWool: ClothingSocks = {
+  category: 'clothing',
+  l10n: {
+    description: 'itemClothingSocksWoolDescription',
+    name: 'itemClothingSocksWoolName',
+  },
+  protection: 0,
+  type: 'socks',
+  variant: 'wool',
+  warmth: 3,
+};
+
+export default socksWool;
