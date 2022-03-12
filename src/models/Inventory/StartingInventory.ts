0c28d3a0a2be1a5fd159fc267907456c101fc73b
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-03-12 22:21:45 +0100

    chore: add worn outfit to starting inventory

--- src/models/Inventory/StartingInventory.ts
+++ src/models/Inventory/StartingInventory.ts
@@ -1,3 +1,11 @@
+import hatBeanieWool from '../Item/Clothing/HatBeanieWool';
+import jacketDenim from '../Item/Clothing/JacketDenim';
+import pantsJeans from '../Item/Clothing/PantsJeans';
+import shirtCotton from '../Item/Clothing/ShirtCotton';
+import shoesSneakers from '../Item/Clothing/ShoesSneakers';
+import socksCotton from '../Item/Clothing/SocksCotton';
+import socksWool from '../Item/Clothing/SocksWool';
+import underwearCotton from '../Item/Clothing/UnderwearCotton';
 import bearBerry from '../Item/Consumable/Bearberry';
 import bottleWater from '../Item/Consumable/BottleWater';
 import crispsTomato from '../Item/Consumable/CrispsTomato';
@@ -6,7 +14,19 @@ import { Inventory } from './types';
 const startingInventory: Inventory = {
   capacity: 30,
   isOpen: false,
-  items: [bearBerry, bottleWater, crispsTomato],
+  items: [
+    bearBerry,
+    bottleWater,
+    crispsTomato,
+    hatBeanieWool,
+    jacketDenim,
+    pantsJeans,
+    shirtCotton,
+    shoesSneakers,
+    socksCotton,
+    socksWool,
+    underwearCotton,
+  ],
 };
 
 export default startingInventory;
