97d9ca479154ad25137fd7d7a26bd98d5064c04e
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-05-11 22:46:30 +0200

    chore: hide animals in fog of war

--- src/renderer/Animal/index.tsx
+++ src/renderer/Animal/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Predator as PredatorInterface } from '~/src/models/Animal/Predator/types';
 import { Prey as PreyInterface } from '~/src/models/Animal/Prey/types';
+import { useSelector } from '~/src/store/hooks';
+import { selectMapFogOfWarPositionVisible } from '~/src/store/reducers/map/selectors';
 
 import Predator from './Predator';
 import Prey from './Prey';
@@ -10,6 +12,14 @@ import { AnimalRendererProps } from './types';
 import './animal.css';
 
 const Animal: React.FC<AnimalRendererProps> = ({ animal, style }) => {
+  const isPositionVisible = useSelector((state) =>
+    selectMapFogOfWarPositionVisible(state, animal.position),
+  );
+
+  if (!isPositionVisible) {
+    return null;
+  }
+
   switch (animal.category) {
     case 'predator':
       return <Predator animal={animal as PredatorInterface} style={style} />;
