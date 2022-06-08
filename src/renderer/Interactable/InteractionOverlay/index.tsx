1139b45722004adb4684dde6e3b7776241ce8f30
Hendrik Volschenk <righteous.trespasser@gmail.com>
2022-06-08 23:17:33 +0200

    chore: move player position calculation into interactable overlay

--- src/renderer/Interactable/InteractionOverlay/index.tsx
+++ src/renderer/Interactable/InteractionOverlay/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import { Position } from '~/src/models/Position';
+import { useSelector } from '~/src/store/hooks';
+import { selectPlayerPosition } from '~/src/store/reducers/player/selectors';
+import positionsEqual from '~/src/utilities/positionsEqual';
+
 import './interaction-overlay.css';
 
 interface InteractinoOverlayAction {
@@ -10,18 +15,32 @@ interface InteractinoOverlayAction {
 
 interface InteractionOverlayProps {
   actions: InteractinoOverlayAction[];
+  position: Position;
 }
 
-const InteractionOverlay: React.FC<InteractionOverlayProps> = ({ actions }) => (
-  <div id="interaction-overlay">
-    <div className="interaction-overlay__content">
-      {actions.map((action) => (
-        <button disabled={action.disabled} key={action.title} onClick={action.action} type="button">
-          {action.title}
-        </button>
-      ))}
-    </div>
-  </div>
-);
+const InteractionOverlay: React.FC<InteractionOverlayProps> = ({ actions, position }) => {
+  const playerPosition = useSelector(selectPlayerPosition);
+
+  if (positionsEqual(position, playerPosition)) {
+    return (
+      <div id="interaction-overlay">
+        <div className="interaction-overlay__content">
+          {actions.map((action) => (
+            <button
+              disabled={action.disabled}
+              key={action.title}
+              onClick={action.action}
+              type="button"
+            >
+              {action.title}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 export default InteractionOverlay;
